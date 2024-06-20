4f0e1fcc6eeff36fc8d1b4cfbd0b2cf62e6a4088
Eugene0028 <evgeniy.flatt@yandex.ru>
2024-06-20 15:15:39 +0700

    refactor pom.xml

--- starter/src/main/java/io/sibguti/starter/config/AuthStarter.java
+++ starter/src/main/java/io/sibguti/starter/config/AuthStarter.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 
 @Configuration
-public class MyStarterAutoConfiguration {
+public class AuthStarter {
     @Autowired
     private Environment environment;
 
